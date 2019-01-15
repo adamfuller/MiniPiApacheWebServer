c9554b8e8e32a5df5dfd231341ad526ebb2f307d
Adam Fuller <alfuller16@gmail.com>
2019-01-14 23:40:51 -0600

    -Adam switch update to prepared statement

--- FileTransferServer/update.php
+++ FileTransferServer/update.php
@@ -5,25 +5,57 @@
         $table = $_REQUEST['table'];
     } elseif (isset($_REQUEST['ex_ip']) && $_REQUEST['ex_ip'] != ''){
         $table = $_REQUEST['ex_ip'];
-    } else {
+    } else{
         $table = "open";
     }
 
+    if (isset($_REQUEST['username'])){
+        $username = $_REQUEST['username'];
+    } else{
+        die("No username");
+    }
+
+    if (isset($_REQUEST['ip'])){
+        $ip = $_REQUEST['ip'];
+    } else{
+        die("No ip");
+    }
+
+    if (isset($_REQUEST['active'])){
+        $active = intval($_REQUEST['active']);
+    } else{
+        die("No active");
+    }
+
+    // mask is available but not necessary it is a byte to be used for 8 booleans on the row
+    $mask = intval($_REQUEST['mask']) ?? 0;
 
     // sql statement
     $labels = $_REQUEST["s"];
     $key = $_REQUEST["k"];
-    $sql = "UPDATE ".$table." SET ".$labels." WHERE ".$key;
+    $sql = "UPDATE ".$table." SET active = ?, mask = ? WHERE (username = ?) AND (ip = ?)";
 
     // attempt to connect to database
     $db = new mysqli("localhost", "root", "JqFl8497__GcZ-P", "FileTransferServer");
+    
 
     // ensure connection to MySQL
     if ($db->connect_errno > 0){
         die("Connection failed");
     }
 
+    $statement = $db->prepare($sql);
+    $statement->bind_param("iiss", $active, $mask, $username, $ip);
+    
+    $result = $statement->execute();
+
     // query sql statement
-    $result = $db->query($sql);
+    if ($result){
+        echo "Succes";
+    } else {
+        echo $db->error;
+    }
+    
+    mysqli_close($db);
     
 ?>

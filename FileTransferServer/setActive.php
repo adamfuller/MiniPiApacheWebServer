56df69ee8054c6b61bdd42e83628b276ef7189c6
Adam Fuller <alfuller16@gmail.com>
2019-01-16 21:40:11 -0600

    -Adam fixed naming mixup

--- FileTransferServer/setActive.php
+++ FileTransferServer/setActive.php
@@ -1,5 +1,4 @@
 <?php
-
     // database settings
     // table open for any traffic/transfers
     if (isset($_REQUEST['table']) && $_REQUEST['table'] != ''){
@@ -10,30 +9,51 @@
         $table = "open";
     }
 
-    // sql statement
-    $sql = "SELECT * from $table WHERE (mask & 1) > 0 ";
+    if (isset($_REQUEST['username'])){
+        $username = $_REQUEST['username'];
+    } else{
+        die("No username");
+    }
 
-    // if (isset($_REQUEST['mask'])){
-    //     $mask = $_REQUEST['mask'];
-    //     $sql = $sql . " where mask = '$mask'";
-    // }
+    if (isset($_REQUEST['ip'])){
+        $ip = str_replace(".", "_", $_REQUEST['ip']);
+    } else{
+        die("No ip");
+    }
+
+    if (isset($_REQUEST['active'])){
+        $active = intval($_REQUEST['active']);
+    } else{
+        die("No active");
+    }
+
+    // sql statement
+    $sql = "UPDATE $table SET active = ?, mask = (mask | 1) WHERE (username = ?) AND (ip = ?)";
 
     // attempt to connect to database
     $db = new mysqli("localhost", "root", "JqFl8497__GcZ-P", "FileTransferServer");
+    
 
     // ensure connection to MySQL
     if ($db->connect_errno > 0){
         die("Connection failed");
     }
 
-    // query sql statement
-    $result = $db->query($sql);
-    $rows = array();
-    while ($r = mysqli_fetch_assoc($result)){
-        $r['ip'] = str_replace("_",".", $r['ip']);
-        $rows['query_results'][] = $r;
-    }
-    echo json_encode($rows);
+    $statement = $db->prepare($sql);
+    $statement->bind_param("iss", $active, $username, $ip);
+    
+    $result = $statement->execute();
 
-    mysqli_close($db);
+    // query sql statement
+    if (!$result){
+        echo $db->error;
+    } elseif ($db->affected_rows > 0) {
+        echo "success";
+    } else {
+        return "failure";
+    }
+    
+    $statement->close();
+    $db->close();
+    
 ?>

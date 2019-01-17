1ffbef60cd6ae50add8783bb0607f36716fb432d
Adam Fuller <alfuller16@gmail.com>
2019-01-16 19:25:41 -0600

    -Adam added makeInactive and getActive

---
+++ FileTransferServer/makeInactive.php
@@ -0,0 +1,59 @@
+<?php
+    // database settings
+    // table open for any traffic/transfers
+    if (isset($_REQUEST['table']) && $_REQUEST['table'] != ''){
+        $table = str_replace(".", "_", $_REQUEST['table']);
+    } elseif (isset($_REQUEST['ex_ip']) && $_REQUEST['ex_ip'] != ''){
+        $table = str_replace(".", "_", $_REQUEST['ex_ip']);
+    } else{
+        $table = "open";
+    }
+
+    if (isset($_REQUEST['username'])){
+        $username = $_REQUEST['username'];
+    } else{
+        die("No username");
+    }
+
+    if (isset($_REQUEST['ip'])){
+        $ip = str_replace(".", "_", $_REQUEST['ip']);
+    } else{
+        die("No ip");
+    }
+
+    if (isset($_REQUEST['active'])){
+        $active = intval($_REQUEST['active']);
+    } else{
+        die("No active");
+    }
+
+    // sql statement
+    $sql = "UPDATE $table SET active = ?, mask = (mask & ~1) WHERE (username = ?) AND (ip = ?)";
+
+    // attempt to connect to database
+    $db = new mysqli("localhost", "root", "JqFl8497__GcZ-P", "FileTransferServer");
+    
+
+    // ensure connection to MySQL
+    if ($db->connect_errno > 0){
+        die("Connection failed");
+    }
+
+    $statement = $db->prepare($sql);
+    $statement->bind_param("iss", $active, $username, $ip);
+    
+    $result = $statement->execute();
+
+    // query sql statement
+    if (!$result){
+        echo $db->error;
+    } elseif ($db->affected_rows > 0) {
+        echo "success";
+    } else {
+        return "failure";
+    }
+    
+    $statement->close();
+    $db->close();
+    
+?>

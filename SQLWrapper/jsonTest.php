cfc1f10df01f0e7270bc7a96dc4be03b809e0698
Adam Fuller <alfuller16@gmail.com>
2019-06-09 23:15:48 -0500

    added identical testing

--- SQLWrapper/jsonTest.php
+++ SQLWrapper/jsonTest.php
@@ -3,7 +3,7 @@
     if ($json_str == null) die("No json input");
     echo ("input : $json_str\n");
     $json = json_decode($json_str);
-    if ($json == null) die("Couldn't decode json");
+    if ($json === null) die("Couldn't decode json");
 
     foreach ($json as $key => $value){
         echo ("$key: $value\n");

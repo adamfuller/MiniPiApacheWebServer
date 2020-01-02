18c44b37372826f301eda8a7019b816286096c05
Adam Fuller <alfuller16@gmail.com>
2020-01-01 19:07:25 -0600

    added the moon and adjusted spacing

---
+++ datetime/index.php
@@ -0,0 +1,5 @@
+<?php 
+date_default_timezone_set("CST"); 
+$date = date('Y/m/d/h:i_a', time());
+echo $date;
+?>
--- planets/webgl.js
+++ planets/webgl.js
@@ -2,58 +2,58 @@
 Each frame is taken as one second in time
 Each pixel is 1 meter wide and 1 meter tall
 */
-let mouseLoc={x:0,y:0};
+let mouseLoc = { x: 0, y: 0 };
 let G = 6.67408e-11;
 let num = 5;
 let planets = []
 let numSuns = 1;
 let suns = [];
-var sunMap,earthMap,venusMap,marsMap,jupiterMap,saturnMap,neptuneMap,uranusMap,plutoMap;
+var sunMap, earthMap, venusMap, marsMap, jupiterMap, saturnMap, neptuneMap, uranusMap, plutoMap, moonMap;
 
 let solarSystem;
 
 
-function addListeners(){
+function addListeners() {
     var canvas = document.getElementById("defaultCanvas0");
-    canvas.addEventListener("mousemove",function(evt){
+    canvas.addEventListener("mousemove", function (evt) {
         let rect = canvas.getBoundingClientRect();
         mouseLoc = {
-            x: evt.clientX - rect.left - width/2,
-            y: evt.clientY - rect.top - height/2
+            x: evt.clientX - rect.left - width / 2,
+            y: evt.clientY - rect.top - height / 2
         };
         //console.log("X: "+mouseLoc.x + " Y: "+ mouseLoc.y);
     })
-    canvas.addEventListener("mouseout",function(evt){
+    canvas.addEventListener("mouseout", function (evt) {
         mouseLoc = {
             x: 0,
             y: 0
         }
     });
 
-    canvas.addEventListener("mousedown", function(evt){
-        createPlanet(mouseLoc.x,-mouseLoc.y,30, true);
+    canvas.addEventListener("mousedown", function (evt) {
+        createPlanet(mouseLoc.x, -mouseLoc.y, 30, true);
     })
 
 }
 
-function velocityForOrbit(M,m,r){
-    return Math.sqrt(G*(M+m)/r);
+function velocityForOrbit(M, m, r) {
+    return Math.sqrt(G * (M + m) / r);
 }
 
-function createPlanet(x,y,z,orbit){
-    planets.push(new Planet(x,y,z,orbit));
+function createPlanet(x, y, z, orbit) {
+    planets.push(new Planet(x, y, z, orbit));
 }
 
-function velocityForOrbit3D(sun, planet){
+function velocityForOrbit3D(sun, planet) {
     // magnitude of the distance from the planet to the sun
     let dista = dist(sun.pos.x, sun.pos.y, sun.pos.z, planet.pos.x, planet.pos.y, planet.pos.z)
 
     // this is the standard gravitational parameter for the sun and the specified planet
-    let GM = G*(sun.mass+planet.mass);
+    let GM = G * (sun.mass + planet.mass);
 
     // magnitude of the velocity neaded for orbit
     // this is the square root of the gravitational parameter divided by the distance
-    let V = Math.sqrt(GM/dista);
+    let V = Math.sqrt(GM / dista);
 
     // magnitudes of the distances from the planet to the sun
     // the direction of this vector is not needed so taking the absolute value will work just as well
@@ -77,16 +77,16 @@ function velocityForOrbit3D(sun, planet){
         The tangent vectors will be denoted by "T" followed by a number to indicate the order of their creation
 
     */
-    
+
     let T1; // this will be a tangent vector to the acceleration
-    if (distZ !== 0 && distY !== 0){
-        T1 = createVector(0,-planetToSunVec.z, planetToSunVec.y);
-    } else if (distY!== 0 && distX !== 0) {
-        T1 = createVector(-planetToSunVec.y,planetToSunVec.x, 0);
-    } else if (distX !== 0 && distZ !== 0){
+    if (distZ !== 0 && distY !== 0) {
+        T1 = createVector(0, -planetToSunVec.z, planetToSunVec.y);
+    } else if (distY !== 0 && distX !== 0) {
+        T1 = createVector(-planetToSunVec.y, planetToSunVec.x, 0);
+    } else if (distX !== 0 && distZ !== 0) {
         T1 = createVector(-planetToSunVec.z, 0, planetToSunVec.x)
     } else {
-        T1 = createVector(-planetToSunVec.y,planetToSunVec.x, 0);
+        T1 = createVector(-planetToSunVec.y, planetToSunVec.x, 0);
     }
 
     // This will be a vector tangent to both the acceleration and E1
@@ -101,7 +101,7 @@ function velocityForOrbit3D(sun, planet){
 
 
     // list of positive unit angles
-    let angs = [0, 1/6, 1/4, 1/3, 1/2, 2/3, 3/4, 5/6, 1];
+    let angs = [0, 1 / 6, 1 / 4, 1 / 3, 1 / 2, 2 / 3, 3 / 4, 5 / 6, 1];
 
     /*
         Since a tangent vector in 3D space is actually a plane, 
@@ -109,7 +109,7 @@ function velocityForOrbit3D(sun, planet){
         For this direction I will pick a random unit angle between 0 and pi
     */
     // picks random unit angle
-    let omega = random(-1,1)*Math.PI;
+    let omega = random(-1, 1) * Math.PI;
 
     /*
 
@@ -128,16 +128,16 @@ function velocityForOrbit3D(sun, planet){
         that must be multiplied by the magnitude of the velocity needed for orbit
         to obtain the final vector
     */
-    let velVec = createVector(vx,vy,vz).mult(V);
+    let velVec = createVector(vx, vy, vz).mult(V);
     return velVec
 }
 
-function forceOfGravity(m1,m2,r){
-    let g = G*m1*m2/(r*r)
+function forceOfGravity(m1, m2, r) {
+    let g = G * m1 * m2 / (r * r)
     return g;
 }
 
-function preload(){
+function preload() {
     sunMap = loadImage("sunmap.jpg");
     venusMap = loadImage("venusmap.jpg");
     earthMap = loadImage("earthmap.jpg");
@@ -147,103 +147,104 @@ function preload(){
     uranusMap = loadImage("uranusmap.jpg")
     neptuneMap = loadImage("neptunemap.jpg");
     plutoMap = loadImage("plutomap.jpg");
-    solarSystem = [marsMap, earthMap, jupiterMap, saturnMap, neptuneMap, uranusMap, plutoMap]
-    
+    moonMap = loadImage("moonmap.jpg");
+    solarSystem = [marsMap, earthMap, jupiterMap, saturnMap, neptuneMap, uranusMap, plutoMap, moonMap];
+
 }
 
-function setup(){
-    createCanvas(800,800,WEBGL);
+function setup() {
+    createCanvas(800, 800, WEBGL);
     perspective();
 
     addListeners();
 
-    for (let s=0;s<numSuns;s++){
-        suns[s] = new Sun(0,0,0, 30000000000000);
+    for (let s = 0; s < numSuns; s++) {
+        suns[s] = new Sun(0, 0, 0, 30000000000000);
     }
 
-    
-    for (let i=0;i<num;i++){
-        planets[i] = new Planet(i*30+30, i*30+30, i*30+30);
+
+    for (let i = 0; i < num; i++) {
+        planets[i] = new Planet(i * 30 + 30, i * 30 + 30, i * 30 + 30);
         // provides a velocity necessary for orbit
-        planets[i].vel = velocityForOrbit3D(suns[0],planets[i]);
+        planets[i].vel = velocityForOrbit3D(suns[0], planets[i]);
     }
 
 }
 
-function draw(){
+function draw() {
     background(0);
     //translate(mouseLoc.x, mouseLoc.y, 0)
 
-    for (let p=planets.length-1;p>=0;p--){
-        if (planets[p].toBeRemoved){
-            planets.splice(planets.indexOf(planets[p]),1);
+    for (let p = planets.length - 1; p >= 0; p--) {
+        if (planets[p].toBeRemoved) {
+            planets.splice(planets.indexOf(planets[p]), 1);
             break
         }
-        
-        for (let s=0;s<suns.length;s++){
+
+        for (let s = 0; s < suns.length; s++) {
             push()
             suns[s].show();
             pop()
-            planets[p].applyGravity(suns[s].mass,suns[s].pos.x,suns[s].pos.y,suns[s].pos.z);
+            planets[p].applyGravity(suns[s].mass, suns[s].pos.x, suns[s].pos.y, suns[s].pos.z);
         }
-        for (let o=planets.length-1;o>=0;o--){
-            if (planets[p] !== planets[o]){
+        for (let o = planets.length - 1; o >= 0; o--) {
+            if (planets[p] !== planets[o]) {
                 planets[p].hasCollided(planets[o]);
                 planets[p].applyGravity(planets[o].mass, planets[o].pos.x, planets[o].pos.y, planets[o].pos.z)
             }
         }
-        
-        if (!planets[p].toBeRemoved){
+
+        if (!planets[p].toBeRemoved) {
             planets[p].update();
-            if ((planets[p].pos.x > width * 2 || planets[p].pos.x < 0 - width * 2) && (planets[p].pos.y > height * 2 || planets[p].pos.y<0-height*2 ) && (planets[p].pos.z > (height+width) || planets[p].pos.z<0-(height+width))){
+            if ((planets[p].pos.x > width * 2 || planets[p].pos.x < 0 - width * 2) && (planets[p].pos.y > height * 2 || planets[p].pos.y < 0 - height * 2) && (planets[p].pos.z > (height + width) || planets[p].pos.z < 0 - (height + width))) {
                 planets[p].toBeRemoved = true;
             }
             planets[p].show();
         }
     }
-    
+
 
 }
 
 
 
-class Planet{
-    constructor(x,y,z, orbit){
-        this.pos = createVector(x||0,y||0,z||0);
-        this.vel = createVector(0,0,0);
-        this.acc = createVector(0,0,0);
-        this.radius = random(10,15);
+class Planet {
+    constructor(x, y, z, orbit) {
+        this.pos = createVector(x || 0, y || 0, z || 0);
+        this.vel = createVector(0, 0, 0);
+        this.acc = createVector(0, 0, 0);
+        this.radius = random(10, 15);
         this.density = 0.001;
-        this.volume = (4/3)*PI*Math.pow(this.radius, 3);
-        this.mass = this.volume*this.density
-        this.spin = Math.random()*PI/30;
+        this.volume = (4 / 3) * PI * Math.pow(this.radius, 3);
+        this.mass = this.volume * this.density
+        this.spin = Math.random() * PI / 30;
         this.spinP = 0;
-        this.spinAxis = [floor(random(2)),1,floor((random(2)))];
+        this.spinAxis = [floor(random(2)), 1, floor((random(2)))];
         this.toBeRemoved = false;
         this.explode = false;
         this.map = solarSystem[floor(random(solarSystem.length))];
-        if (orbit){
-            this.vel = velocityForOrbit3D(suns[0],this)
+        if (orbit) {
+            this.vel = velocityForOrbit3D(suns[0], this)
         }
     }
 
-    update(){
-            this.vel.add(this.acc);
-            this.pos.add(this.vel);
-            this.acc.mult(0);
-    
+    update() {
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+
     };
 
-    applyForce(force){
+    applyForce(force) {
         this.acc.add(force);
     }
 
-    applyGravity(M,Mx,My,Mz){
-        let dis = Math.sqrt((Mx-this.pos.x)*(Mx-this.pos.x)+(My-this.pos.y)*(My-this.pos.y)+(Mz-this.pos.z)*(Mz-this.pos.z));
+    applyGravity(M, Mx, My, Mz) {
+        let dis = Math.sqrt((Mx - this.pos.x) * (Mx - this.pos.x) + (My - this.pos.y) * (My - this.pos.y) + (Mz - this.pos.z) * (Mz - this.pos.z));
         let Fg = forceOfGravity(M, this.mass, dis);
 
         // x direction from the other mass to this
-        let xDir = Math.sign(Mx-this.pos.x);
+        let xDir = Math.sign(Mx - this.pos.x);
 
         // magnitude of x distance 
         let xDis = Math.abs(Mx - this.pos.x);
@@ -264,24 +265,24 @@ class Planet{
         //  F = directionX * (distanceX / distanceTotal) * Force / mass
         //
         // Adds a=F/m for each direction
-        this.acc.x+=xDir*xDis/dis*Fg/this.mass;
-        this.acc.y+=yDir*yDis/dis*Fg/this.mass;
-        this.acc.z+=zDir*zDis/dis*Fg/this.mass;
+        this.acc.x += xDir * xDis / dis * Fg / this.mass;
+        this.acc.y += yDir * yDis / dis * Fg / this.mass;
+        this.acc.z += zDir * zDis / dis * Fg / this.mass;
     }
 
-    hasCollided(otherPlanet){
+    hasCollided(otherPlanet) {
         let distance = dist(this.pos.x, this.pos.y, this.pos.z, otherPlanet.pos.x, otherPlanet.pos.y, otherPlanet.pos.z);
 
         //two planets collide
-        if (distance < this.radius + otherPlanet.radius){
-            if (this.mass<otherPlanet.mass){
+        if (distance < this.radius + otherPlanet.radius) {
+            if (this.mass < otherPlanet.mass) {
                 this.toBeRemoved = true;
             }
-            if (this.mass>otherPlanet.mass){
-                this.density += otherPlanet.density/10;
-                this.radius += otherPlanet.radius/20;
+            if (this.mass > otherPlanet.mass) {
+                this.density += otherPlanet.density / 10;
+                this.radius += otherPlanet.radius / 20;
             }
-            
+
             //console.log("collision at :" + this.pos.x + "," + this.pos.y + ',' + this.pos.z);
             //console.log(otherPlanet.pos.x, otherPlanet.pos.y, otherPlanet.pos.z);
             //console.log(distance)
@@ -289,29 +290,29 @@ class Planet{
 
     }
 
-    show(){
+    show() {
         push();
         texture(this.map);
         translate(this.pos.x, -this.pos.y, this.pos.z);
-        rotate(this.spin*this.spinP, this.spinAxis);
+        rotate(this.spin * this.spinP, this.spinAxis);
         this.spinP++;
-        if (this.spin>=2*PI){
-            this.spinP=0;
+        if (this.spin >= 2 * PI) {
+            this.spinP = 0;
         }
         sphere(this.radius);
         pop();
     }
 
-    
+
 }
 
 
-class Sun{
-    constructor(x,y,z,mass){
-        this.pos = createVector(x||0,y||0,z||0);
-        this.mass = mass||300000000000;
+class Sun {
+    constructor(x, y, z, mass) {
+        this.pos = createVector(x || 0, y || 0, z || 0);
+        this.mass = mass || 300000000000;
     }
-    show(){
+    show() {
         push();
         texture(sunMap);
         translate(this.pos.x, -this.pos.y, this.pos.z);

7b1596334d7f918acad05b9a3b4ce981e5262784
Adam Fuller <alfuller16@gmail.com>
2020-01-02 15:16:45 -0600

    initial database creation test

---
+++ SQL/createDatabase.php
@@ -0,0 +1,35 @@
+<?php
+// No database selected
+include "../tools.php";
+
+// database settings
+    // table open for any traffic/transfers
+
+    $database = getInput(array("database","db", "d"), die("No database specified"));
+    $user = getInput(array("user","u"), die("No user specified"));
+    $host = getInput(array("host", "url", "ip"), "localhost");
+    $password = getInput(array("password","psk", "p"), die("No password specified"));
+
+    // sql statement
+    $sql = "CREATE DATABASE $database";
+
+    // attempt to connect to database
+    $db = new mysqli($host, $user, $password);
+    
+    // ensure connection to MySQL
+    if ($db->connect_errno > 0){
+        die("Connection failed");
+    }
+    
+    $result = $db->query($sql);
+
+    // query sql statement
+    if ($result){
+        echo "true";
+    } else {
+        echo $db->error;
+    }
+    
+    $db->close();
+
+?>

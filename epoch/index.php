5663eed1041066f02561073b65f41b45833ee55d
Adam Fuller <alfuller16@gmail.com>
2020-01-02 12:23:40 -0600

    added timezones, fixed link

--- epoch/index.php
+++ epoch/index.php
@@ -1,4 +1,31 @@
 <?php 
-date_default_timezone_set("UTC"); 
-echo "UTC:".time(); 
+
+$tz = getInput("timezone") ?? getInput("tz") ?? "UTC";
+
+date_default_timezone_set($tz); 
+echo "$tz:".time();
+
+function getInput($name){
+    if (isset($_REQUEST[$name]) && $_REQUEST[$name] != ''){
+        return $_REQUEST[$name];
+    } else{
+        return NULL;
+    }
+}
+
+
+function jsonify($keys, $values){
+    $output = array();
+
+    if (count($keys) != count($values)){
+        return NULL;
+    }
+
+    for ($i = 0; $i < count($keys); $i++){
+        $output[$keys[$i]] = $values[$i];
+    }
+
+    return json_encode($output);
+}
+
 ?>

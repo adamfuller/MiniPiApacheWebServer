7ec59a286540e80db8be71c464376bb04d096bcc
Adam Fuller <alfuller16@gmail.com>
2020-01-02 12:49:13 -0600

    added centralized tools.php with jsonify and getInput

--- datetime/index.php
+++ datetime/index.php
@@ -1,33 +1,10 @@
 <?php 
+include "../tools.php";
 
-$tz = getInput("timezone") ?? getInput("tz") ?? "CST";
+$tz = getInput(array("timezone","tz"), "UTC");
 
 date_default_timezone_set($tz); 
 $date = date('Y/m/d/h:i_a', time());
 echo jsonify(array("datetime", "timezone"), array($date, $tz));
-
-
-function getInput($name){
-    if (isset($_REQUEST[$name]) && $_REQUEST[$name] != ''){
-        return $_REQUEST[$name];
-    } else{
-        return NULL;
-    }
-}
-
-
-function jsonify($keys, $values){
-    $output = array();
-
-    if (count($keys) != count($values)){
-        return NULL;
-    }
-
-    for ($i = 0; $i < count($keys); $i++){
-        $output[$keys[$i]] = $values[$i];
-    }
-
-    return json_encode($output);
-}
 
 ?>

2e8fab9796cd8b305826d2c859055b775bda92f9
Adam Fuller <alfuller16@gmail.com>
2020-01-02 12:19:55 -0600

    JSON output, added timezone option

--- datetime/index.php
+++ datetime/index.php
@@ -1,5 +1,33 @@
 <?php 
-date_default_timezone_set("CST"); 
+
+$tz = getInput("timezone") ?? getInput("tz") ?? "CST";
+
+date_default_timezone_set($tz); 
 $date = date('Y/m/d/h:i_a', time());
-echo $date;
+echo jsonify(array("datetime", "timezone"), array($date, $tz));
+
+
+function getInput($name){
+    if (isset($_REQUEST[$name]) && $_REQUEST[$name] != ''){
+        return $_REQUEST[$name];
+    } else{
+        return NULL;
+    }
+}
+
+
+function jsonify($keys, $values){
+    $output = array();
+
+    if (count($keys) != count($values)){
+        return NULL;
+    }
+
+    for ($i = 0; $i < count($keys); $i++){
+        $output[$keys[$i]] = $values[$i];
+    }
+
+    return json_encode($output);
+}
+
 ?>

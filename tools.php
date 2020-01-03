1aac8061e9c98d346190ff7bda34fd4bcd6b8b2a
Adam Fuller <alfuller16@gmail.com>
2020-01-02 21:19:06 -0600

    added JSON parsing to getInput with getJsonInput

--- tools.php
+++ tools.php
@@ -1,22 +1,58 @@
 <?php 
 
+$json_str = file_get_contents('php://input');
+$json_input = NULL;
+if ($json_str != NULL) $json_input = json_decode($json_str);
+
 /**
  * @param array/string $names possible names of input to retrieve
  * @param any $default value returned if input isn't present
- * @return any value retrieved from $_REQUEST[$name]
+ * @return any value retrieved from $_REQUEST[$name] or attached JSON
  */
 function getInput($names, $default = NULL, $dieOnFail = FALSE){
     $names = is_array($names) ? $names : array($names);
 
+    // Check to see if any of the names are present
     foreach($names as $name){
         if (isset($_REQUEST[$name])){
             return $_REQUEST[$name];
         }
     }
 
-    if ($dieOnFail){
+    // If we are supposed to die with failures and don't have the json input
+    // then die.
+    if ($dieOnFail && !isset($json_input)){
         die("Failed to get: " . $names[0]);
     }
+
+    // Check the json input if present and return it
+    $json_value = getJsonInput($names, $default, $dieOnFail);
+    if (isset($json_value)) return $json_value;
+
+    return $default;
+}
+
+function getJsonInput($names, $default = NULL, $dieOnFail = FALSE){
+    global $json_str;
+    global $json_input;
+
+    if ($json_str == NULL){
+        if ($dieOnFail) die("No JSON input");
+        return $default;
+    }
+
+    if ($json_input == NULL){
+        if ($dieOnFail) die("Couldn't decode JSON");
+        return $default;
+    } 
+
+    foreach($names as $name){
+        if (isset($json_input->{$name})){
+            return $json_input->{$name};
+        }
+    }
+
+    if ($dieOnFail) die("Could not find input labeled: " . $names[0]);
 
     return $default;
 }
@@ -42,4 +78,3 @@ function jsonify($keys, $values){
 
     return json_encode($output);
 }
-?>

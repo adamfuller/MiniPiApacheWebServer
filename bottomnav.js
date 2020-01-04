f4b3067a360caaee6dc9ebb07c8c3a3ef404b7b4
Adam Fuller <alfuller16@gmail.com>
2020-01-04 15:22:31 -0600

    added bottom navigation text

---
+++ bottomnav.js
@@ -0,0 +1,30 @@
+// Grab the current URL and then remove the http://
+var currentURL = document.URL.replace("http://", "");
+
+// Split up the path
+var subs = currentURL.split("/");
+
+// Add a line break
+document.body.appendChild(document.createElement("br"));
+
+// Iterate through all the path sections
+for (let i = 0; i < subs.length; i++) {
+    // make a new anchor element
+    var elem = document.createElement("a");
+
+    // name it the last part of the path add back in the missing /
+    elem.innerHTML = subs[i] + "/";
+
+    var ref = "";
+    // Iterate through the rest of the url to build the reference
+    for (let j = 0; j <= i; j++) ref += subs[j] + "/";
+
+    // Assign the reference
+    elem.href = ref;
+
+    // Make it all green
+    elem.style.color = "rgb(39, 240, 21)";
+
+    // Add it to the page
+    document.body.appendChild(elem);
+}

8770b23a3af9e57d96a5605f9e884d8f108ed70c
Adam Fuller <alfuller16@gmail.com>
2020-01-04 15:28:22 -0600

    remove extra / from end

--- bottomnav.js
+++ bottomnav.js
@@ -9,15 +9,18 @@ document.body.appendChild(document.createElement("br"));
 
 // Iterate through all the path sections
 for (let i = 0; i < subs.length; i++) {
+
+    if (subs[i] == '') continue;
+
     // make a new anchor element
     var elem = document.createElement("a");
 
     // name it the last part of the path add back in the missing /
     elem.innerHTML = subs[i] + "/";
 
-    var ref = "";
+    var ref = "http://";
     // Iterate through the rest of the url to build the reference
-    for (let j = 0; j <= i; j++) ref += subs[j] + "/";
+    for (let j = 0; j <= i; j++) ref += subs[j] + (subs[j] != '' ? "/" : '');
 
     // Assign the reference
     elem.href = ref;

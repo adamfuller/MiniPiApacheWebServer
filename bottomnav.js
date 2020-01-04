0358991346d6d16259ee0ed03fe75ed585bf157a
Adam Fuller <alfuller16@gmail.com>
2020-01-04 15:50:14 -0600

    wait for window load event

--- bottomnav.js
+++ bottomnav.js
@@ -1,33 +1,35 @@
-// Grab the current URL and then remove the http://
-var currentURL = document.URL.replace("http://", "");
+window.addEventListener('load', function () {
+    // Grab the current URL and then remove the http://
+    var currentURL = document.URL.replace("http://", "");
 
-// Split up the path
-var subs = currentURL.split("/");
+    // Split up the path
+    var subs = currentURL.split("/");
 
-// Add a line break
-document.body.appendChild(document.createElement("br"));
+    // Add a line break
+    document.body.appendChild(document.createElement("br"));
 
-// Iterate through all the path sections
-for (let i = 0; i < subs.length; i++) {
+    // Iterate through all the path sections
+    for (let i = 0; i < subs.length; i++) {
 
-    if (subs[i] == '') continue;
+        if (subs[i] == '') continue;
 
-    // make a new anchor element
-    var elem = document.createElement("a");
+        // make a new anchor element
+        var elem = document.createElement("a");
 
-    // name it the last part of the path add back in the missing /
-    elem.innerHTML = subs[i] + "/";
+        // name it the last part of the path add back in the missing /
+        elem.innerHTML = subs[i] + "/";
 
-    var ref = "http://";
-    // Iterate through the rest of the url to build the reference
-    for (let j = 0; j <= i; j++) ref += subs[j] + (subs[j] != '' ? "/" : '');
+        var ref = "http://";
+        // Iterate through the rest of the url to build the reference
+        for (let j = 0; j <= i; j++) ref += subs[j] + (subs[j] != '' ? "/" : '');
 
-    // Assign the reference
-    elem.href = ref;
+        // Assign the reference
+        elem.href = ref;
 
-    // Make it all green
-    elem.style.color = "rgb(39, 240, 21)";
+        // Make it all green
+        elem.style.color = "rgb(39, 240, 21)";
 
-    // Add it to the page
-    document.body.appendChild(elem);
-}
+        // Add it to the page
+        document.body.appendChild(elem);
+    }
+})

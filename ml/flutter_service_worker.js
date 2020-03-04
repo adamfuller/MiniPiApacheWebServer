fe6ecf247cb3b225c9fa5225575f1ee44c7984d6
Adam Fuller <alfuller16@gmail.com>
2020-03-03 22:18:25 -0600

    updated to most recent version, added possible anti-cache

--- ml/flutter_service_worker.js
+++ ml/flutter_service_worker.js
@@ -2,11 +2,11 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "61b7719527881ef63ebfbdfb6c63c83d",
-"/main.dart.js": "17ef41d60b86fae2934ae0d83d07f936",
+"/main.dart.js": "7eaa39333906a0d904335fd7a95568ab",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "90429e89de58cfca8f025ed8b460774a",
-"/assets/LICENSE": "1a80be6c5724a31e6f9c9e06dba53b63",
+"/assets/LICENSE": "2858a6dcc58080e5389ea38aa9657ac7",
 "/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
@@ -32,9 +32,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
